b8c0f68e462cab9f6d9f10ccf89376bee127cdf3
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-12-23 06:35:06 +0200

    _v1b

--- index.js
+++ index.js
@@ -1,138 +1,132 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-const mainRequest = async (url, nesting) => {
-  let emails = [];
+// https://madappgang.com
 
-  // GETTING LINKS LIST
-  const getLinks = async (url, nesting) => {
-    // CHECKING THE END OF RECURSION
-    if (nesting === 0) {
-      return;
-    }
-
-    // CURRENT FETCH REQUEST
-    const getHTML = async (currentUrl) => {
-      try {
-        let validationUrl = validURL(currentUrl);
-
-        if (validationUrl) {
-          console.log("FETCHING", currentUrl);
-          const { data } = await axios.get(currentUrl, {
-            headers: {
-              "Accept-Encoding": "text/html", //application/json
-            },
-          });
-          if (data !== undefined) {
-            return cheerio.load(data);
-          }
-        } else {
-          return;
-        }
-      } catch (error) {}
-    };
-
-    // LOADING DATA TO CHEERIO
-    const $ = await getHTML(url);
-    if (!$) {
-      return;
-    }
-    const linkObjects = $("a");
-    const links = [];
-
-    linkObjects.each((index, element) => {
-      // EXTRACTING LINKS FROM THE PAGE CODE
-      const urlFromHtml = $(element).attr("href");
-      let urlFromHtmlCleaned = undefined;
-      let newCurrentUrl = undefined;
-
-      if (
-        urlFromHtml !== undefined &&
-        urlFromHtml !== "" &&
-        urlFromHtml !== "/" &&
-        !urlFromHtml.includes("tel") &&
-        !urlFromHtml.includes("#") &&
-        !urlFromHtml.includes("http")
-      ) {
-        // CLEANING UP URL ADDRESS FROM PAGE HTML
-        if (urlFromHtml.charAt(urlFromHtml.length - 1) === "/") {
-          urlFromHtmlCleaned = urlFromHtml.slice(0, -1);
-        } else {
-          urlFromHtmlCleaned = urlFromHtml;
-        }
-
-        // CLEANING UP CURRENT FETCHED URL ADDRESS
-        if (url.charAt(url.length - 1) === "/") {
-          newCurrentUrl = url.slice(0, -1);
-        } else {
-          newCurrentUrl = url;
-        }
-
-        // GETTING EMAILS
-        if (urlFromHtml.includes("mailto")) {
-          emails.push(urlFromHtml.replace("mailto:", ""));
-        }
-
-        // ADDING LINKS TO AN ARRAY
-        if (!urlFromHtml.includes("mailto")) {
-          links.push(
-            newCurrentUrl + urlFromHtmlCleaned // get the href attribute
-          );
-        }
-      }
-    });
-
-    // SORTING LINKS AND DELETING UNNECESSARY ONES
-    const uniqueLinks = [...new Set(links)].sort();
-    if (uniqueLinks.length === 0) {
-      return;
-    }
-
-    // RECURSION THROUGH LINKS
-    function iterate(index) {
-      if (index === uniqueLinks.length) {
-        return;
-      }
-
-      // console.log("Process", uniqueLinks[index], "nesting", nesting - 1);
-      getLinks(uniqueLinks[index], nesting - 1);
-
-      iterate(index + 1);
-    }
-
-    iterate(0);
-
-    return emails;
-  };
-
-  const resultEmails = await getLinks(url, nesting);
-
-  const uniqueEmails = [...new Set(resultEmails)];
-  return uniqueEmails;
-};
-
-function validURL(str) {
-  const pattern = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-      "(\\#[-a-z\\d_]*)?$",
-    "i"
-  ); // fragment locator
-
-  return !!pattern.test(str);
-}
+const visited = new Set();
+const toVisit = new Set();
 
 const actionIndex = process.argv.indexOf("myapp");
+// to insert nesting level from CL. Not Active now !!!!
+// const nestingFromCL = process.argv[actionIndex + 2];
+const baseUrl = process.argv[actionIndex + 1];
+let emails = [];
 
-// RECEIVING ARGUMENTS FROM COMMAND LINE AND STARTING PROCESS
+if (actionIndex !== -1) {
+  toVisit.add(baseUrl);
+} else {
+  console.log("There are no valid URL !");
+}
+
+// Extracting urls and emails from html document with cheerio
+const extractLinks = ($) => {
+  const linkObjects = $("a");
+  const links = [];
+
+  linkObjects.each((index, element) => {
+    // EXTRACTING LINKS FROM THE PAGE CODE
+    const urlFromHtml = $(element).attr("href");
+    let urlFromHtmlCleaned = undefined;
+
+    if (
+      urlFromHtml !== undefined &&
+      urlFromHtml !== "" &&
+      urlFromHtml !== "/" &&
+      !urlFromHtml.includes("tel") &&
+      !urlFromHtml.includes("#") &&
+      !urlFromHtml.includes("http")
+    ) {
+      // CLEANING UP URL ADDRESS FROM PAGE HTML
+      if (urlFromHtml.charAt(1) === "/") {
+        urlFromHtmlCleaned = urlFromHtml.slice(0, -1);
+      } else {
+        urlFromHtmlCleaned = urlFromHtml;
+      }
+
+      // GETTING EMAILS
+      if (urlFromHtml.includes("mailto")) {
+        emails.push(urlFromHtml.replace("mailto:", ""));
+      }
+
+      // ADDING LINKS TO AN ARRAY
+      if (!urlFromHtml.includes("mailto")) {
+        links.push(
+          baseUrl + urlFromHtmlCleaned // get the href attribute
+        );
+      }
+    }
+  });
+
+  return [...new Set(links)];
+};
+
+async function request(links, nesting) {
+  return Promise.allSettled(links.map((el) => crawl(el, nesting - 1)));
+}
+
+// ----------------------------
+// NESTING LEVEL IS SET HERE ->
+const crawl = async (url, nesting = 3) => {
+  try {
+    if (nesting < 1) {
+      return;
+    }
+    visited.add(url);
+
+    // ------------------------------
+    // TO SEE THE PROGRESS!!!!!
+    // console.log("nestLvl", nesting, "URL- ", url);
+    // ------------------------------
+
+    const { data } = await axios.get(url, {
+      headers: {
+        "Accept-Encoding": "text/html", //application/json
+      },
+    });
+
+    const $ = cheerio.load(data);
+    const links = extractLinks($);
+
+    if (links) {
+      links
+        .filter((link) => !visited.has(link)) // Filter out already visited links
+        .forEach((link) => toVisit.add(link));
+    }
+
+    toVisit.delete(url);
+    const toVisitArray = [...toVisit];
+
+    if (toVisitArray.length > 0) {
+      const _ = await request(toVisitArray, nesting);
+    }
+
+    // ------------------------------
+    // Liniar Iteration ( for of ) without NESTING !
+    // ------------------------------
+    // for (const nextLink of toVisit.values()) {
+    //   if (nesting < 1) {
+    //     console.log("nesting break");
+    //     break;
+    //   }
+
+    //   await crawl(nextLink);
+    // }
+  } catch (error) {}
+};
+
 (async () => {
-  if (actionIndex !== -1) {
-    const nesting = process.argv[actionIndex + 1];
-    const url = process.argv[actionIndex + 2];
-    const finalEmails = await mainRequest(url, nesting);
-    console.log("WEBSITE EMAILS", finalEmails);
-  }
+  console.log("We're starting, please wait.");
+  //------------ this one to receive nesting level from CL. Doesn't active now!!!
+  // const data = await crawl(baseUrl, nestingFromCL);
+  const _ = await crawl(baseUrl);
+
+  // ------------------------------
+  // SHOWING THE DATA
+  // console.log(toVisit);
+  console.log(`----- ${[...toVisit].length} |LINKS TO VISIT----`);
+  // console.log(visited);
+  console.log(`-------${[...visited].length} | VISITED LINKS -------`);
+  console.log("---------------------------------");
+  console.log("-------------EMAILS-------------");
+  console.log([...new Set(emails)]);
 })();
